68fab67f2d54f7700dacb364afb3efc04cb03fa6
xt.hu <xt.hu@sunmedia.com.cn>
2021-03-11 14:17:21 +0800

    fix merge,Marlin use UART0,printer successful

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -3017,7 +3017,7 @@ DaveX plan for Teensylu/printrboard-type pinouts (ref teensylu & sprinter) for a
 #define Z_DIR_PIN          34   //GPIOP4_2
 #define Z_ENABLE_PIN       33   //GPIOP4_1
 #define Z_MIN_PIN          24   //GPIOP3_0
-#define Z_MAX_PIN          -1   //
+#define Z_MAX_PIN          -1
 
 #define E0_STEP_PIN        26   //GPIOP3_2
 #define E0_DIR_PIN         28   //GPIOP3_4
@@ -3029,13 +3029,13 @@ DaveX plan for Teensylu/printrboard-type pinouts (ref teensylu & sprinter) for a
 #define HEATER_0_PIN       17   //GPIOP2_1
 
 #undef HEATER_BED_PIN
-#define HEATER_BED_PIN     21    //GPIOP2_5
+#define HEATER_BED_PIN     21   //GPIOP2_5
 
 #undef FAN_PIN
-#define FAN_PIN			   19  //GPIOP2_3
+#define FAN_PIN            19   //GPIOP2_3
 
 #define TEMP_I2C_SDA       13   //GPIOP1_5
-#define TEMP_I2C_SCL       12  //GPIOP1_4
+#define TEMP_I2C_SCL       12   //GPIOP1_4
 
 #undef TEMP_0_PIN
 #undef TEMP_BED_PIN
